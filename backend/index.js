31423fa14978b930c37b2e28be96b06545aef6f4
NadaBoukhari <51091307+NadaBoukhari@users.noreply.github.com>
2020-12-09 14:08:33 +0100

    added dotenv config

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const pool = require("./database");
+const pool = require("../database");
 const app = express();
 const cors = require("cors");
 const port = 3000;
